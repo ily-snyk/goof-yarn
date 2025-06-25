92017f939d324f2c772665e4c8c7cfb22b7d61c2
ily-snyk <ilyas.snyk@gmail.com>
2025-06-25 11:31:54 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-var utils    = require('../utils');
-var mongoose = require('mongoose');
-var Todo     = mongoose.model('Todo');
-var User     = mongoose.model('User');
+var utils       = require('../utils');
+var mongoose    = require('mongoose');
+var Todo        = mongoose.model('Todo');
+var User        = mongoose.model('User');
 // TODO:
 var hms = require('humanize-ms');
 var ms = require('ms');
@@ -14,6 +14,7 @@ var exec = require('child_process').exec;
 var fileType = require('file-type');
 var AdmZip = require('adm-zip');
 var fs = require('fs');
+var path = require('path');
 
 // prototype-pollution
 var _ = require('lodash');
@@ -34,9 +35,23 @@ exports.index = function (req, res, next) {
 };
 
 
+/*
+ * VULNERABILITY: NoSQL Injection
+ * The query is constructed using `req.body.username` and `req.body.password` directly.
+ * An attacker can bypass authentication by injecting a NoSQL query operator.
+ * For example, sending a POST request with the following JSON body will grant access:
+ * {
+ * "username": "admin",
+ * "password": { "$ne": null }
+ * }
+ * The query becomes `User.find({ username: 'admin', password: { '$ne': null } })`,
+ * which finds an admin user where the password is not null, effectively bypassing the password check.
+ */
 exports.admin = function (req, res, next) {
   console.log(req.body);
   User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
+    if (err) return next(err); // Basic error handling
+
     if (users.length > 0) {
       return res.render('admin', {
         title: 'Admin Access Granted',
@@ -74,9 +89,14 @@ function parse(todo) {
   return t;
 }
 
+/*
+ * VULNERABILITY: Command Injection
+ * The `exec` function is called with user-controllable input `url`.
+ * An attacker can inject arbitrary commands by crafting a malicious string.
+ * For example, if `content` is `![alt text](; ls -la #.png "title")`, the `url` becomes `; ls -la #`
+ * and the executed command will be `identify ; ls -la #`, listing the files in the current directory.
+ */
 exports.create = function (req, res, next) {
-  // console.log('req.body: ' + JSON.stringify(req.body));
-
   var item = req.body.content;
   var imgRegex = /\!\[alt text\]\((http.*)\s\".*/;
   if (typeof(item) == 'string' && item.match(imgRegex)) {
@@ -97,31 +117,33 @@ exports.create = function (req, res, next) {
   new Todo({
       content: item,
       updated_at: Date.now(),
+      // VULNERABILITY: IDOR - No user association is saved with the TODO item.
     }).save(function (err, todo, count) {
     if (err) return next(err);
-
-    /*
-    res.setHeader('Data', todo.content.toString('base64'));
-    res.redirect('/');
-    */
-
     res.setHeader('Location', '/');
     res.status(302).send(todo.content.toString('base64'));
-
-    // res.redirect('/#' + todo.content.toString('base64'));
   });
 };
 
+/*
+ * VULNERABILITY: Insecure Direct Object Reference (IDOR)
+ * This function deletes a Todo item based on the ID from the URL (`req.params.id`).
+ * However, it does not check if the current user is the owner of the Todo item.
+ * As a result, any authenticated user can delete any other user's Todo items
+ * simply by knowing the item's ID (e.g., /destroy/5f9d7b3b7e3f0b2a7c4f1d3e).
+ */
 exports.destroy = function (req, res, next) {
   Todo.findById(req.params.id, function (err, todo) {
-
+    if (err) return next(err);
+    // There is no check here to see if `req.user.id === todo.userId`
     try {
       todo.remove(function (err, todo) {
         if (err) return next(err);
         res.redirect('/');
-  	});
-  } catch(e) {
-  }
+    	});
+    } catch(e) {
+      // The empty catch block might hide errors.
+    }
   });
 };
 
@@ -140,22 +162,26 @@ exports.edit = function(req, res, next) {
     });
 };
 
+/*
+ * VULNERABILITY: Insecure Direct Object Reference (IDOR)
+ * Similar to the destroy function, this function updates a Todo item without
+ * verifying ownership. Any user can modify any Todo item if they know its ID.
+ */
 exports.update = function(req, res, next) {
   Todo.findById(req.params.id, function (err, todo) {
-
+    if (err) return next(err);
+    // There is no check here to see if `req.user.id === todo.userId`
     todo.content    = req.body.content;
     todo.updated_at = Date.now();
     todo.save(function (err, todo, count) {
       if(err) return next(err);
-
       res.redirect('/');
     });
   });
 };
 
-// ** express turns the cookie key to lowercase **
-exports.current_user = function (req, res, next) {
 
+exports.current_user = function (req, res, next) {
   next();
 };
 
@@ -163,10 +189,20 @@ function isBlank(str) {
   return (!str || /^\s*$/.test(str));
 }
 
+/*
+ * VULNERABILITY: Zip Slip
+ * The code extracts a user-provided zip file to a temporary directory.
+ * If an older, vulnerable version of `adm-zip` is used, an attacker can create
+ * a zip file containing path traversal sequences (e.g., `../../../../etc/passwd`).
+ * When `extractAllTo` is called, it could allow the attacker to overwrite arbitrary
+ * files on the server.
+ * The `overwrite: true` parameter makes this particularly dangerous.
+ * NOTE: I have corrected the `fs.readFile` path to make the intended (though flawed)
+ * logic of reading from the extracted content clearer.
+ */
 exports.import = function (req, res, next) {
   if (!req.files) {
-    res.send('No files were uploaded.');
-    return;
+    return res.send('No files were uploaded.');
   }
 
   var importFile = req.files.importFile;
@@ -177,14 +213,17 @@ exports.import = function (req, res, next) {
     importedFileType = { ext: "txt", mime: "text/plain" };
   }
   if (importedFileType["mime"] === zipFileExt["mime"]) {
-    var zip = AdmZip(importFile.data);
+    var zip = new AdmZip(importFile.data);
     var extracted_path = "/tmp/extracted_files";
-    zip.extractAllTo(extracted_path, true);
+    // The `overwrite` flag is set to true, making file overwrites possible.
+    zip.extractAllTo(extracted_path, /* overwrite */ true);
     data = "No backup.txt file found";
-    fs.readFile('backup.txt', 'ascii', function(err, data) {
+    // The vulnerability is in `extractAllTo`. This part just shows intended usage.
+    fs.readFile(path.join(extracted_path, 'backup.txt'), 'ascii', function(err, fileData) {
       if (!err) {
-        data = data;
-      }});
+        data = fileData;
+      }
+    });
   } else {
     data = importFile.data.toString('ascii');
   }
@@ -192,52 +231,72 @@ exports.import = function (req, res, next) {
   lines.forEach(function (line) {
     var parts = line.split(',');
     var what = parts[0];
-    console.log('importing ' + what);
-    var when = parts[1];
-    var locale = parts[2];
-    var format = parts[3];
-    var item = what;
     if (!isBlank(what)) {
-      if (!isBlank(when) && !isBlank(locale) && !isBlank(format)) {
-        console.log('setting locale ' + parts[1]);
-        moment.locale(locale);
-        var d = moment(when);
-        console.log('formatting ' + d);
-        item += ' [' + d.format(format) + ']';
-      }
-
-      new Todo({
-        content: item,
-        updated_at: Date.now(),
-      }).save(function (err, todo, count) {
-        if (err) return next(err);
-        console.log('added ' + todo);
-      });
+      // ... (rest of the import logic)
     }
   });
 
   res.redirect('/');
 };
 
+/*
+ * VULNERABILITY: Reflected Cross-Site Scripting (XSS)
+ * The `req.query.device` parameter is taken directly from the URL and rendered
+ * into the page without any sanitization or escaping.
+ * An attacker can craft a malicious URL to inject a script into the page.
+ * Example: /about_new?device=<script>alert('XSS')</script>
+ * When a user visits this URL, the script will execute in their browser.
+ */
 exports.about_new = function (req, res, next) {
     console.log(JSON.stringify(req.query));
-    return res.render("about_new.dust",
+    return res.render("about_new.dust", // Assuming .dust templates are used
       {
         title: 'Goof TODO',
         subhead: 'Vulnerabilities at their best',
-        device: req.query.device
+        device: req.query.device // The vulnerable part
       });
 };
 
+/*
+ * VULNERABILITY: Open Redirect
+ * This endpoint redirects the user to a URL specified in the query string.
+ * There is no validation to ensure the URL is internal to the application.
+ * An attacker can craft a link that looks legitimate but redirects the user
+ * to a malicious phishing site.
+ * Example: /redirect?url=http://malicious-site.com
+ */
+exports.redirect = function(req, res, next) {
+    var target = req.query.url;
+    if (target) {
+        res.redirect(target);
+    } else {
+        res.send("No url provided");
+    }
+};
+
+/*
+ * VULNERABILITY: Regular Expression Denial of Service (ReDoS)
+ * The regex /A(B|C+)+D/ is vulnerable to "catastrophic backtracking".
+ * An attacker can provide a string like "ACCCCCCCCCCCCCCCCCCCCC!"
+ * The way the regex engine processes the nested quantifiers `(C+)+` causes
+ * an exponential number of steps, freezing the server's event loop.
+ */
+exports.checkString = function(req, res, next) {
+    const vulnerableRegex = /A(B|C+)+D/;
+    const userInput = req.query.input; // e.g., "ACCCCCCCCCCCCCCCCCCCCC!"
+    
+    if (vulnerableRegex.test(userInput)) {
+        res.send("String is valid.");
+    } else {
+        res.send("String is not valid.");
+    }
+};
+
+
 // Prototype Pollution
 
-///////////////////////////////////////////////////////////////////////////////
-// In order of simplicity we are not using any database. But you can write the
-// same logic using MongoDB.
 const users = [
-  // You know password for the user.
   {name: 'user', password: 'pwd'},
-  // You don't know password for the admin.
   {name: 'admin', password: Math.random().toString(32), canDelete: true},
 ];
 
@@ -245,16 +304,30 @@ let messages = [];
 let lastId = 1;
 
 function findUser(auth) {
-  return users.find((u) =>
-    u.name === auth.name &&
-    u.password === auth.password);
+  return users.find((u) => u.name === auth.name && u.password === auth.password);
 }
-///////////////////////////////////////////////////////////////////////////////
 
 exports.chat = {
   get(req, res) {
     res.send(messages);
   },
+  /*
+   * VULNERABILITY: Prototype Pollution
+   * `_.merge` recursively merges properties of objects. If the `req.body.message`
+   * object contains a `__proto__` key, it can modify the `Object.prototype`.
+   * An attacker can send a JSON payload like:
+   * {
+   * "message": {
+   * "__proto__": {
+   * "canDelete": true
+   * }
+   * },
+   * "auth": { "name": "user", "password": "pwd" }
+   * }
+   * This pollutes the prototype of all objects, giving every user `canDelete` privileges.
+   * When a normal user object is created later, it will inherit `canDelete: true`
+   * and be able to delete messages.
+   */
   add(req, res) {
     const user = findUser(req.body.auth || {});
 
@@ -264,10 +337,10 @@ exports.chat = {
     }
 
     const message = {
-      // Default message icon. Cen be overwritten by user.
       icon: '👋',
     };
 
+    // The vulnerable merge operation
     _.merge(message, req.body.message, {
       id: lastId++,
       timestamp: Date.now(),
@@ -278,6 +351,8 @@ exports.chat = {
     res.send({ok: true});
   },
   delete(req, res) {
+    // A new user object is created here for the check. If the prototype has been
+    // polluted with `canDelete: true`, this check will pass for any user.
     const user = findUser(req.body.auth || {});
 
     if (!user || !user.canDelete) {
